442a0774b4fecafb1264c8a1bdd24fc8891e734c
jt-manful <johnmanful2002@hotmail.com>
2024-04-01 17:33:12 +0000

    db commit

---
+++ db.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE "final_project";
+use "final_project";
+
+-- Creation of SmartNodes Table
+CREATE TABLE SmartNodes (
+    NodeID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Location VARCHAR(255) NOT NULL
+);
+
+-- Creation of SensorTypes Table
+CREATE TABLE SensorTypes (
+    TypeID INT AUTO_INCREMENT PRIMARY KEY,
+    TypeName VARCHAR(255) NOT NULL
+);
+
+-- Creation of SensorData Table
+CREATE TABLE SensorData (
+    DataID INT AUTO_INCREMENT PRIMARY KEY,
+    NodeID INT,
+    TypeID INT,
+    Value DECIMAL(10,2) NOT NULL,
+    Timestamp DATETIME NOT NULL,
+    FOREIGN KEY (NodeID) REFERENCES SmartNodes(NodeID),
+    FOREIGN KEY (TypeID) REFERENCES SensorTypes(TypeID)
+);
